0c91f7d4b48589d76b33f2bc28ffd15316448fac
Michael Perreault <mpaulperr@gmail.com>
2021-08-11 16:45:57 +0200

    added gitignores

--- rust/codewars/src/main.rs
+++ rust/codewars/src/main.rs
@@ -1,26 +1,20 @@
-// _________________________6 kyu_Bouncing Balls______________________________
-https://www.codewars.com/kata/5544c7a5cb454edb3c000047
+
+// //Bouncing Balls
+// https://www.codewars.com/kata/5544c7a5cb454edb3c000047
+
+// //Roman Numerals Encoder
+// https://www.codewars.com/kata/51b62bf6a9c58071c600001b
+
+// //Reverse words
+// https://www.codewars.com/kata/5259b20d6021e9e14c0010d4
 
 
+// Convert a Boolean to a String
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd
+// fn boolean_to_string(b: bool) -> String {
 
-fn boolean_to_string(b: bool) -> String {
+// }
 
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn example() {
-        assert_eq!(boolean_to_string(true), "true", "When we pass in true, we want the string \"true\" as output");
-        assert_eq!(boolean_to_string(false), "false", "When we pass in false, we want the string \"false\" as output");
-        assert_eq!(boolean_to_string(false), "false", "When we pass in false, we want the string \"false\" as output");
-    }
-}
-
-// _________________________8 kyu_Convert a Boolean to a String______________________________
-// Implement a function which convert the given boolean value into its string representation.
-
-// Note: Only valid inputs will be given.
+//         assert_eq!(boolean_to_string(true), "true", "When we pass in true, we want the string \"true\" as output");
+//         assert_eq!(boolean_to_string(false), "false", "When we pass in false, we want the string \"false\" as output");
+//         assert_eq!(boolean_to_string(false), "false", "When we pass in false, we want the string \"false\" as output");
