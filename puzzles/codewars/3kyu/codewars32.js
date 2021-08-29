7cb03603f5a5ed95614d73555c74f36eaa7082f9
fattie <mpaulperr@gmail.com>
2021-08-29 15:59:35 +0200

    battleship!

--- puzzles/codewars/3kyu/codewars32.js
+++ puzzles/codewars/3kyu/codewars32.js
@@ -1,62 +1,80 @@
 function validateBattlefield(field) {
-    // [battleship,cruiser,destroyer,submarine] - we want [1,2,3,4] at the end
-    let ships = [0,0,0,0]
     let ships = {
         '4': 0,
         '3': 0,
         '2': 0,
         '1': 0
     };
-    //so we don't find & count the same ship multiple times
-    let saved = [];
 
-    for (let y in field) {    //for each y
-        for (let x in y) {  //for each x in each y
-            if (saved.indexOf([x,y]) != -1) continue //if already logged the ship's location
-            if (field[y][x] === 1) { //if a new ship is found
-                let direction = directional_check(a, b, field);
-                switch (direction) {
-                    case 'illegal': return false
-                    case 'right':
-                        while (field[a][b] === 1) {
-                            a += 1;
-                            ship_length += 1;
-                            savedX.push(a);
-                            savedY.push(b);
-                        }
-                        break;
-                    case 'down':
-                        while (field[a][b] === 1) {
-                            b += 1;
-                            ship_length += 1;
-                            savedX.push(a);
-                            savedY.push(b);
-                        }
-                        break;
+    let saved = [];
+    for (let y = 0; y < field.length; y++) {
+        for (let x = 0; x < field[y].length; x++) {
+        if (saved.indexOf(y.toString() + x.toString()) != -1) continue //if already logged the ship's location, go next
+            if (field[y][x] === 1) {
+                console.log(y,x,'---found a ship here');
+                let ship_length = 0;
+                let a = x;
+                let b = y; 
+                let face = direction(a,b,field);
+                if (face === 'illegal') return false
+                if (face === 'sub') {
+                    ship_length += 1;
+                } else {
+                    while (field[b][a] === 1) {
+                        ship_length += 1;
+                        if (!diagonals(a,b,field)) return false
+                        saved.push(b.toString() + a.toString());
+                        if (face === 'right') a += 1;
+                        if (face === 'down') b += 1;
+                    }
                 }
-                ships[count_ship(ship_length)] += 1;
+                ships[ship_length.toString()] += 1
+                console.log(ships);
             }
         }
     }
-    return ships === [1,2,3,4]
+
+    return valid_check(ships);
 }
 
-let directional_check = (x, y, field) => {
-    let a = x;
-    let b = y;
-    let ship_length = 1;
-    let right = field[x+1][y];
-    let down = field[x][y+1];
-    if ((field[x-1][y+1] + field[x-1][y] + field[x-1][y-1] + field[x][y-1] + field[x+1][y-1] + field[x+1][y+1]) != 0) return 'illegal'// confirm no neighbors besides down/right
-    if (right + down === 0) return 1
-    else if (right === 1 && down === 0) {
-        return 'right'
-    } else if (right === 0 && down === 1) {
-        return 'down'
-    } else return 'illegal'
+let diagonals = (x, y, field) => {
+    //always check downright, check the other diagonals conditionally depending on if at border or not.
+    let arr = [];
+    arr.push(field[y+1][x+1]);
+    if (x > 0) arr.push(field[y+1][x-1]);
+    if (y > 0) arr.push(field[y-1][x+1]);
+    if (x > 0 && y > 0) arr.push(field[y-1][x-1]);
+
+    for (let i of arr) {
+        if (arr[i] === 1) return false
+    }
+    //if all diagonal directions are undefined or 0,
+    return true
 }
 
-let count_ship = (n) => ships[n.toString()] += 1
+let direction = (x, y, field) => {
+    let r = field[y][x+1];
+    let d = field[y+1][x];
+
+    if (r + d === 0) return 'sub'
+    else if (r === 1 && d === 0) return 'right'
+    else if (r === 0 && d === 1) return 'down'
+    else return 'illegal'
+}
+
+let valid_check = (ships) => {
+    let valid = {
+        '4': 1,
+        '3': 2,
+        '2': 3,
+        '1': 4
+    }
+    console.log(ships,'ships',valid,'valid');
+    for (let x = 1; x < 5; x++) {
+        if (ships[x.toString()] != valid[x.toString()]) return false
+    }
+    return true
+}
 
 console.log(validateBattlefield(
     [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -69,7 +87,7 @@ console.log(validateBattlefield(
      [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ]), 'expected "true"');
+    ]), 'expected true');
 
 // Write a method that takes a field for well-known board game "Battleship" 
 // as an argument and returns true if it has a valid disposition of ships, false otherwise. 
