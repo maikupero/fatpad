35a55b50b9ba0145a88296b85d8d4b2fe42f596e
fattie <mpaulperr@gmail.com>
2021-08-30 15:12:31 +0200

    cpp

---
+++ puzzles/codewars/7kyu/codewars33.cpp
@@ -0,0 +1,26 @@
+#include <string>
+bool solution(std::string const &str, std::string const &ending) {
+    return true;
+}
+
+    Assert::That(solution("abcde", "cde"), Equals(true));
+    Assert::That(solution("abcde", "abc"), Equals(false));
+    Assert::That(solution("abc", ""), Equals(true));
+
+// Complete the solution so that it returns true if the first argument(string) 
+// passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+//Test:
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     cout << "Hello World" << endl;
+// }
