e84b1ccac291f17228bcdf8706c7710c5acdbaf0
fattie <mpaulperr@gmail.com>
2021-08-31 15:46:43 +0200

    linked lists and readme update

---
+++ puzzles/leetcode/learninglinkedlists.js
@@ -0,0 +1,42 @@
+/*
+so a linked list is just 
+data + pointer to the next data + pointer
+
+nodes of two attributes: values and next pointers.
+*/
+
+class ListNode {
+    constructor(val, next) {
+        this.val = 0; 
+        this.next = null; 
+    };
+};
+
+let string_to_list = (string) => {
+    let head = new ListNode(); 
+    let current = head; 
+
+    for (let x in string) { 
+        current.val = string[x];
+        if (x < string.length) {
+            current.next = new ListNode();
+            current = current.next;
+        }// thanks brother i appreciate
+    }
+
+    return head
+}
+
+let print_list = (node) => {
+    while (node.next != null) {
+        console.log(node);  
+        node = node.next;
+    }
+}
+
+let headnode = string_to_list('Hello, world!');
+
+// console.log(headnode, '<-- headnode');
+print_list(headnode);
+
+
