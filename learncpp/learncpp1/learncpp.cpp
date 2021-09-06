823937225241f19d3c77077d0e660d6ab287f5e7
fattie <mpaulperr@gmail.com>
2021-09-06 13:30:05 +0200

    cpp work

--- learncpp/learncpp1/learncpp.cpp
+++ learncpp/learncpp1/learncpp.cpp
@@ -4,13 +4,9 @@
 
 int main()
 {
-    std::cout << "Enter two numbers separated by a space: ";
-
     int x;
-    int y;
-    std::cin >> x >> y;
-
-    std::cout << "You entered " << x << " and " << y << '\n';
+    
+    std::cout << x;
     return 0;
 }
 
