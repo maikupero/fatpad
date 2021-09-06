4e03e04e77fb78956bbede0c5787f6d90fdb3379
fattie <mpaulperr@gmail.com>
2021-09-06 13:10:54 +0200

    a

---
+++ learncpp/learncpp1/learncpp.cpp
@@ -0,0 +1,16 @@
+// code-runner Run in terminal
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter two numbers separated by a space: ";
+
+    int x;
+    int y;
+    std::cin >> x >> y;
+
+    std::cout << "You entered " << x << " and " << y << '\n';
+    return 0;
+}
+
