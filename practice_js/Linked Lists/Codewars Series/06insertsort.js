88d8b090136115df1b54a1adaed4f8b8d843146b
fattie <mpaulperr@gmail.com>
2021-09-09 14:49:34 +0200

    boo yaaa

--- practice_js/Linked Lists/Codewars Series/06insertsort.js
+++ practice_js/Linked Lists/Codewars Series/06insertsort.js
@@ -1,7 +1,20 @@
-// ------------------ Linked Lists - Sorted Insert ------------------
-// https://www.codewars.com/kata/linked-lists-sorted-insert
+// ------------------ Linked Lists - Insert Sort ------------------
+// https://www.codewars.com/kata/linked-lists-insert-sort
 
-// ---------- For 05 Sorted Insert ----------
+// ---------- For 06 Insert Sort ----------
+function insertSort(head) {
+    let current = head;
+    head = null;
+
+    while (current) {
+        head = sortedInsert(head, current.data);
+        current = current.next;
+    }
+    
+    return head
+}
+
+// ---------- From 05 Sorted Insert ----------
 function sortedInsert(head, data) {
     if (!head || data <= head.data) return push(head, data)
 
@@ -14,15 +27,6 @@ function sortedInsert(head, data) {
     return head
 }
 
-  
-// Post-submission 04
-function insertNth(head, index, data) {
-    if (index == 0) return push(head, data)
-    head.next = insertNth(head.next, index - 1, data);
-
-    return head;
-}
-
 // // ---------- From 01 Push & Build ----------
 function push(head, data) {
     let push = new Node();
@@ -33,7 +37,7 @@ function push(head, data) {
 }
 
 function buildOneTwoThree() {
-    return push(push(push(null, 3), 2), 1);
+    return push(push(push(null, 2), 1), 3);
 }
 
 // // ---------- Defining a Node in a Linked List ----------
@@ -44,17 +48,20 @@ class Node {
     }
 }
 
-console.log(sortedInsert(buildOneTwoThree(), 4), 'expected 1->2->3->4->null');
+console.log(insertSort(buildOneTwoThree()), 'expected 1->2->3->null');
 
+// Write an InsertSort() function which rearranges nodes in a linked list so they are sorted 
+// in increasing order. You can use the SortedInsert() function that you created in the 
+// "Linked Lists - Sorted Insert" kata below. The InsertSort() function takes the head 
+// of a linked list as an argument and must return the head of the linked list.
 
-// Write a SortedInsert() function which inserts a node into the correct location 
-// of a pre-sorted linked list which is sorted in ascending order. 
-// SortedInsert takes the head of a linked list and data used to create a node as arguments. 
-// SortedInsert() should also return the head of the list.
+// var list = 4 -> 3 -> 1 -> 2 -> null
+// insertSort(list) === 1 -> 2 -> 3 -> 4 -> null
 
-// sortedInsert(1 -> 2 -> 3 -> null, 4) === 1 -> 2 -> 3 -> 4 -> null)
-// sortedInsert(1 -> 7 -> 8 -> null, 5) === 1 -> 5 -> 7 -> 8 -> null)
-// sortedInsert(3 -> 5 -> 9 -> null, 7) === 3 -> 5 -> 7 -> 9 -> null)
+// If the passed in head node is null or a single node, return null or the single node, respectively. 
+// You can assume that the head node will always be either null, a single node, 
+// or a linked list consisting of multiple nodes.
+
 // The push() and buildOneTwoThree() functions do not need to be redefined.
 
 // Related Kata in order of expected completion (increasing difficulty):
@@ -62,8 +69,8 @@ console.log(sortedInsert(buildOneTwoThree(), 4), 'expected 1->2->3->4->null');
 // Linked Lists - Length & Count                   
 // Linked Lists - Get Nth Node                      
 // Linked Lists - Insert Nth Node               
-// Linked Lists - Sorted Insert                     <-- Here
-// Linked Lists - Insert Sort
+// Linked Lists - Sorted Insert
+// Linked Lists - Insert Sort                     <-- Here
 // Linked Lists - Append
 // Linked Lists - Remove Duplicates
 // Linked Lists - Move Node
