015e75304a642edd335ddb0fb8078dbdce9a4697
fattie <mpaulperr@gmail.com>
2021-10-02 11:38:46 +0200

    cs50 week 2

---
+++ cs50x/week2/lecture/uppercase/uppercase.c
@@ -0,0 +1,57 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// int main(void)
+// {
+//     string s = get_string("Before: ");
+//     printf("After: ");
+
+//     for (int i = 0, n = strlen(s); i < n; i++)
+//     {
+//         if (s[i] >= 'a' && s[i] <= 'z') 
+//         {
+//             printf("%c", s[i] - 32);
+//         }
+//         else 
+//         {
+//             printf("%c", s[i]);
+//         }
+//     }
+//     printf("\n");
+// }
+
+// or cty
+
+// int main(void)
+// {
+//     string s = get_string("Before: ");
+//     printf("After: ");
+
+//     for (int i = 0, n = strlen(s); i < n; i++)
+//     {
+//         if (islower(s[i]))
+//         {
+//             printf("%c", toupper(s[i]));
+//         }
+//         else 
+//         {
+//             printf("%c", s[i]);
+//         }
+//     }
+//     printf("\n");
+// }
+
+// or even better. remember it only accepts a single character as an argument.
+int main(void)
+{
+    string s = get_string("Before: ");
+    printf("After: ");
+
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        printf("%c", toupper(s[i]));
+    }
+    printf("\n");
+}
