bb1465de460d5119ae81e3c2794fa90545abcdc9
fattie <mpaulperr@gmail.com>
2021-10-03 17:08:20 +0200

    quiz app

--- projects/Florinpop/quiz/script.js
+++ projects/Florinpop/quiz/script.js
@@ -1,25 +1,25 @@
 const quizData = [
     {
-        question: 'How old is Janice?',
-        a: '60',
-        b: '61',
-        c: '62',
-        d: '63',
-        correct: 'c'
+        question: 'Who is the Emperor of TheBoys?',
+        a: 'Wolfgang',
+        b: 'Fropy',
+        c: 'Bottica',
+        d: 'There is no Emperor of TheBoys.',
+        correct: 'a'
     }, {
-        question: 'What is the worst hero in Dota 2?',
+        question: 'Which garbage hero should you hate the most?',
         a: 'Techies',
-        b: 'Mirana',
-        c: 'Viper',
+        b: 'Shadow Fiend',
+        c: 'Windrunner',
         d: 'Tinker',
         correct: 'a'
     }, {
-        question: 'Who is the president of the United States?',
-        a: 'Jo Mama',
-        b: 'Joe Biden',
-        c: 'Donald Trump',
-        d: 'Joaquin Phoenix',
-        correct: 'b'
+        question: 'If TheBoys play 4 games of Dota 2, how many wins do they procure?',
+        a: '1',
+        b: '2',
+        c: '3',
+        d: '4',
+        correct: 'd'
     }, {
         question: 'Who will win TI10?',
         a: 'OG',
@@ -35,19 +35,25 @@ const quizData = [
         d: '2021',
         correct: 'b'
     }
-]
+];
 
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+    deselectAnswers();
+
     const currentQuizData = quizData[currentQuiz];
     
     questionEl.innerText = currentQuizData.question;
@@ -55,7 +61,43 @@ function loadQuiz() {
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
-
-    currentQuestion++;
 }
 
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    //check to see the answer
+    const answer = getSelected();
+    
+    console.log(answer);
+
+    if(answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+        currentQuiz++;
+        if(answer) {
+            if (currentQuiz < quizData.length) {
+                loadQuiz();
+            } else {
+                quiz.innerHTML = `<h2>You answered ${score}/${quizData.length} questions correctly.</h2> <button onClick="location.reload()">Reload</button>`;
+            }
+        }
+    }
+});
