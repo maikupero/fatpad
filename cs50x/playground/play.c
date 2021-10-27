094f36704e3ba2a3c03b91bc6cbaa97d45091167
fattie <mpaulperr@gmail.com>
2021-10-27 17:42:26 -0400

    ha

--- cs50x/playground/play.c
+++ cs50x/playground/play.c
@@ -1,97 +1,11 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 
-void mergeSort();
-void printArray();
-
-//Main Function
-int main()
+int main(void)
 {
-    int arr[] = {85, 24, 63, 45, 17, 31, 96, 50};
-    int arr_size = sizeof(arr)/sizeof(arr[0]);
-    printf("Given array is [");
-    printArray(arr, arr_size);
-    printf("]\n");
-    mergeSort(arr, 0, arr_size - 1);
-    printf("Sorted array is [");
-    printArray(arr, arr_size);
-    printf("]\n");
-    return 0;
-}
-
-// Merge Function
-void merge(int arr[], int l, int m, int r)
-{
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    int L[n1], R[n2];
-
-    for (i = 0; i < n1; i++)
-    {
-        L[i] = arr[l + i];
-    }
-    for (j = 0; j < n2; j++)
-    {
-        R[j] = arr[m + 1+ j];
-    }
-
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= R[j])
-        {
-            arr[k] = L[i];
-            i++;
-        }
-        else
-        {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < n1)
-    {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-    
-    while (j < n2)
-    {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-
-void mergeSort(int arr[], int l, int r)
-{
-    if (l < r)
-    {
-        int m = l+(r-l)/2;
-        mergeSort(arr, l, m);
-        mergeSort(arr, m+1, r);
-        merge(arr, l, m, r);
-    }
-}
-
-void printArray(int A[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-    {
-        if (i == 7)
-        {
-            printf("%d", A[i]);
-        }
-        else
-        {
-            printf("%d, ", A[i]);
-        }
-    }
+    int i = 5 / 2.0;
+    printf("%d\n", i);
+    i = round(i);
+    printf("%i\n", i);
+    printf("type: %s", typeof i);
 }
