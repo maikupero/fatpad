ff5831e6d90aca630c052553d6f201605973bff5
fattie <mpaulperr@gmail.com>
2021-10-27 17:42:29 -0400

    back

---
+++ cs50x/playground/codewars/7kyu/string.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#include <string.h>
+
+int main(void)
+{
+    bool solution();
+    return 0;h
+}
+
+bool solution(const char *string, const char *ending)
+{
+    return true;
+}
