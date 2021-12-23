aa81f9b756128e92862957d523c594ebab28906e
fattie <mpaulperr@gmail.com>
2021-12-23 15:02:41 -0500

    daily codewars

--- practice/py/decode_morse.py
+++ practice/py/decode_morse.py
@@ -2,28 +2,24 @@
 # --------------------------- Enlightening solutions ----------------------------
 # -------------------------------------------------------------------------------
 
-def formatted_number(n):
-    return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
-
 # -------------------------------------------------------------------------------
 # -------------------------------- My solution ----------------------------------
 # -------------------------------------------------------------------------------
 
-def create_phone_number(n):
-    number = "("
-    for i in range(3):
-        number += str(n[i])
-    number += ") "
-    for i in range(3,6):
-        number += str(n[i])
-    number += "-"
-    for i in range(6,10):
-        number += str(n[i])
+def decodeMorse(morse_code):
+    morse_code = morse_code.split(' ')
+    english = ""
     
-    return number
+    for i in range(len(morse_code)):
+        if morse_code[i] != "":
+            english += MORSE_CODE[morse_code[i]]
+        else:
+            if i > 0 and (morse_code[i-1] != morse_code[i]):
+                english += ' '
+    return english.strip()
 
-test = create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
-expect = "(123) 456-7890"
+test = decodeMorse('.... . -.--   .--- ..- -.. .')
+expect = "HEY JUDE"
 print(f"Tested: {test}\nExpect: {expect}")
 
-# https://www.codewars.com/kata/525f50e3b73515a6db000b83
+# https://www.codewars.com/kata/54b724efac3d5402db00065e
