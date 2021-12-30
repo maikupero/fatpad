1a2386fa805dee706c113b5d2a4fc50d03fcc46a
fattie <mpaulperr@gmail.com>
2021-12-30 16:00:43 -0500

    codewars blah!

--- practice/py/get_pins.py
+++ practice/py/get_pins.py
@@ -7,83 +7,102 @@
 # -------------------------------------------------------------------------------
 # -------------------------------- My solution ----------------------------------
 # -------------------------------------------------------------------------------
-# First impressions:
 def get_pins(observed):
-    buffered_keypad = [
-        ["a","a","a","a","a"],
-        ["a","1","2","3","a"],
-        ["a","4","5","6","a"],
-        ["a","7","8","9","a"],
-        ["a","a","0","a","a"]
-    ]
-    possible = list()
+    keypad = {
+        "1": ['1','2','4'],
+        "2": ['1','2','3','5'],
+        "3": ['2','3','6'],
+        "4": ['1','4','5','7'],
+        "5": ['2','4','5','6','8'],
+        "6": ['3','5','6','9'],
+        "7": ['4','7','8'],
+        "8": ['5','7','8','9','0'],
+        "9": ['6','8','9']
+    }
+    # To do - recursive solution using this dict basically. 
+    # think about what i'm sending in to the function as arguments
 
-    for digit in observed:
-        # Get the location of our current digit
-        for y in range(5):
-            for x in range(5):
-                if buffered_keypad[y][x] == digit:
-                    coords = (y,x)
 
-        # note all its neighbors
-        neighbors = list()
-        if buffered_keypad[coords[0]-1][coords[1]] != "a":
-            neighbors.append(buffered_keypad[coords[0]-1][coords[1]])
-        if buffered_keypad[coords[0]][coords[1]-1] != "a":
-            neighbors.append(buffered_keypad[coords[0]][coords[1]-1])
-        neighbors.append(buffered_keypad[coords[0]][coords[1]])
-        if buffered_keypad[coords[0]][coords[1]+1] != "a":
-            neighbors.append(buffered_keypad[coords[0]][coords[1]+1])
-        if buffered_keypad[coords[0]+1][coords[1]] != "a":
-            neighbors.append(buffered_keypad[coords[0]+1][coords[1]])
 
-        possible.append(neighbors)
 
-    # for digit in possible:
-    print(possible)
-    final_results = possible[0]
-    for x in range(len(possible)):
-        for digit in final_results:
-            digit += 
-    else:
-        for x in range(len(possible)):
-            for digit in possible[x]:
 
-    elif len(possible) == 2:
-        # For every first press
-        # For length of pin code (number of presses)
-        for i in possible[0]:
-            for j in possible[1]:
-                if (i+j) not in final_results:
-                    final_results.append(i+j)
-        return final_results
-    elif len(possible) == 3:
-        for i in possible[0]:
-            for j in possible[1]:
-                for k in possible[2]:
-                    if (i+j+k) not in final_results:
-                        final_results.append(i+j+k)
-        return final_results
-    elif len(possible) == 4:
-        for i in possible[0]:
-            for j in possible[1]:
-                for k in possible[2]:
-                    for l in possible[3]:
-                        if (i+j+k+l) not in final_results:
-                            final_results.append(i+j+k+l)
-        return final_results
+
+
+# First attempt (FAILURE):
+# def get_pins(observed):
+#     buffered_keypad = [
+#         ["a","a","a","a","a"],
+#         ["a","1","2","3","a"],
+#         ["a","4","5","6","a"],
+#         ["a","7","8","9","a"],
+#         ["a","a","0","a","a"]
+#     ]
+#     possible = list()
+
+#     for digit in observed:
+#         # Get the location of our current digit
+#         for y in range(5):
+#             for x in range(5):
+#                 if buffered_keypad[y][x] == digit:
+#                     coords = (y,x)
+
+#         # note all its neighbors
+#         neighbors = list()
+#         if buffered_keypad[coords[0]-1][coords[1]] != "a":
+#             neighbors.append(buffered_keypad[coords[0]-1][coords[1]])
+#         if buffered_keypad[coords[0]][coords[1]-1] != "a":
+#             neighbors.append(buffered_keypad[coords[0]][coords[1]-1])
+#         neighbors.append(buffered_keypad[coords[0]][coords[1]])
+#         if buffered_keypad[coords[0]][coords[1]+1] != "a":
+#             neighbors.append(buffered_keypad[coords[0]][coords[1]+1])
+#         if buffered_keypad[coords[0]+1][coords[1]] != "a":
+#             neighbors.append(buffered_keypad[coords[0]+1][coords[1]])
+
+#         possible.append(neighbors)
+
+#     # for digit in possible:
+#     print(possible)
+#     final_results = possible[0]
+#     for x in range(len(possible)):
+#         for digit in final_results:
+#             digit += 
+#     else:
+#         for x in range(len(possible)):
+#             for digit in possible[x]:
+
+#     elif len(possible) == 2:
+#         # For every first press
+#         # For length of pin code (number of presses)
+#         for i in possible[0]:
+#             for j in possible[1]:
+#                 if (i+j) not in final_results:
+#                     final_results.append(i+j)
+#         return final_results
+#     elif len(possible) == 3:
+#         for i in possible[0]:
+#             for j in possible[1]:
+#                 for k in possible[2]:
+#                     if (i+j+k) not in final_results:
+#                         final_results.append(i+j+k)
+#         return final_results
+#     elif len(possible) == 4:
+#         for i in possible[0]:
+#             for j in possible[1]:
+#                 for k in possible[2]:
+#                     for l in possible[3]:
+#                         if (i+j+k+l) not in final_results:
+#                             final_results.append(i+j+k+l)
+#         return final_results
         
-
-
 test = '8'
 expected = ['5','7','8','9','0']
 test2 = '11'
 expected2 = ["11", "22", "44", "12", "21", "14", "41", "24", "42"]
 test3 = '369'
 expected3 = ["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"]
-# print(f"Tested: {get_pins(test)}\nExpect: {expected}")
+print(f"Tested: {get_pins(test)}\nExpect: {expected}")
 # print(f"Tested: {get_pins(test2)}\nExpect: {expected2}")
-print(f"Tested: {get_pins(test3)}\nExpect: {expected3}")
+# print(f"Tested: {get_pins(test3)}\nExpect: {expected3}")
 
 # Alright, detective, one of our colleagues successfully observed our target person, Robby the robber. 
 # We followed him to a secret warehouse, where we assume to find all the stolen stuff. 
