0668bc1b2eac663d70132a7ce18e1a0c99c10770
fattie <mpaulperr@gmail.com>
2022-02-07 13:41:29 -0500

    cs50 week 9 update

---
+++ practice/leetcode/top100/easy4.js
@@ -0,0 +1,59 @@
+// Best solution:
+
+// Solution for their specific prompt which fails sequences if different order at any point:
+
+// My simple solution for checking right amounts in right order: 
+/*
+* @param {number[]} nums
+* @param {number} target
+* @return {number}
+*/
+ var searchInsert = function(nums, target) {
+    
+};
+// Given Examples
+exnums1 = [1,3,5,6]
+extarg1 = 5
+output1 = 2
+exnums2 = [1,3,5,6]
+extarg2 = 2
+output2 = 1
+exnums3 = [1,3,5,6]
+extarg3 = 7
+output3 = 4
+
+console.log(`Example 1: ${exnums1, extarg1} yields: ${isValid(exnums1, extarg1)}, expected: ${output1}`);
+console.log(`Example 2: ${exnums2, extarg2} yields: ${isValid(exnums2, extarg2)}, expected: ${output2}`);
+console.log(`Example 3: ${exnums3, extarg3} yields: ${isValid(exnums3, extarg3)}, expected: ${output3}`);
+
+// My created test cases
+test1 = "({{}})"
+expected1 = true
+test2 = "({[}])"
+expected2 = false
+test3 = "({[]}}"
+expected3 = false
+console.log(`Test 1: ${test1} yields: ${isValid(test1)}, expected: ${expected1}`)
+console.log(`Test 2: ${test2} yields: ${isValid(test2)}, expected: ${expected2}`)
+console.log(`Test 3: ${test3} yields: ${isValid(test3)}, expected: ${expected3}`)
+
+// Failed this test case because the prompt is unclear. Give me more examples. What. 
+// Knowing this would be failed, I would've just coded in a queue stack.
+failed = "([)]"
+failresult = false
+console.log(`Failed: ${failed} yields: ${isValid(failed)}, expected: ${failresult}`);
+
+
+// The prompt
+// https://leetcode.com/problems/search-insert-position/
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+// If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Constraints:
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums contains distinct values sorted in ascending order.
+// -104 <= target <= 104
+
