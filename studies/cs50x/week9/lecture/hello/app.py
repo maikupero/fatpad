73b9b456f54e808fda577a9e4bee8c62e5c1f846
fattie <mpaulperr@gmail.com>
2022-02-07 13:41:40 -0500

    cs50 week9 update

---
+++ studies/cs50x/week9/lecture/hello/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/greet", methods=["POST"])
+def greet():
+    return render_template("greet.html", name=request.form.get("name", "world"))
+
+
+# http://127.0.0.1:5000/?name=Michael add on  the /... part and it dynamically updates name.
+#  so we make a /greet to essentially do that for us.
+
+# request.args - get
+# request.forms - post
