dcb311e0947beadc56121343656449fe3001b18b
fattie <mpaulperr@gmail.com>
2022-03-11 14:00:13 -0500

    simple py script

--- scripts/rename.py
+++ scripts/rename.py
@@ -1,8 +1,11 @@
 import os
 
+path = input("Enter path to folder (or just hit enter for current dir): ")
+if path == "": path = "."
+
 search = input("Enter text to be replaced: ")
 replace = input("Enter replacement text: ")
 
-for file in os.listdir("."):
+for file in os.listdir(path):
     if search in file:
         os.rename(file, file.replace(f"{search}", f"{replace}"))
