5583ea6b40a69644b17d2bf16b84395832d7a8ce
fattie <mpaulperr@gmail.com>
2022-03-30 15:54:02 -0400

    Invert Binary Tree

--- practice/leetcode/top100/17.py
+++ practice/leetcode/top100/17.py
@@ -50,7 +50,6 @@ expected3 = []
  
 # 226. Invert Binary Tree
 # https://leetcode.com/problems/invert-binary-tree/
-
 # Given the root of a binary tree, invert the tree, and return its root.
 
 # Constraints:
