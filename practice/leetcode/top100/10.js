edd927b6ea7d779c6437b9456e6e25864948436f
fattie <mpaulperr@gmail.com>
2022-03-30 15:57:35 -0400

    Best Time to Buy and Sell Stock

--- practice/leetcode/top100/10.js
+++ practice/leetcode/top100/10.js
@@ -57,15 +57,6 @@ var maxProfitSlow = function(prices) {
     return current
 };
 
-// Prompt
-// You are given an array prices where prices[i] is the price of a given stock on the ith day.
-// You want to maximize your profit by choosing a single day to buy one stock and 
-// choosing a different day in the future to sell that stock.
-// Return the maximum profit you can achieve from this transaction. 
-// If you cannot achieve any profit, return 0.
-// Constraints:
-// 1 <= prices.length <= 105
-// 0 <= prices[i] <= 104
 
 // Examples
 
@@ -86,5 +77,13 @@ console.log(`Example 3: ${test3} yields: ${maxProfit(test3)}, expected: ${expect
 
 
 
-// Failed test cases
-
+// 121. Best Time to Buy and Sell Stock
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and 
+// choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. 
+// If you cannot achieve any profit, return 0.
+// Constraints:
+// 1 <= prices.length <= 105
+// 0 <= prices[i] <= 104
