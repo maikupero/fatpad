51b6fa991c52504729290b731d98813cc38f514a
fattie <mpaulperr@gmail.com>
2022-03-30 15:49:00 -0400

    Jump Game

--- practice/leetcode/top100/39.py
+++ practice/leetcode/top100/39.py
@@ -27,7 +27,6 @@ print(f"Testing {test2} returns {Solution.canJump(test2)} expected {expected2}")
 # You are given an integer array nums. You are initially positioned at the array's first index, 
 # and each element in the array represents your maximum jump length at that position.
 # Return true if you can reach the last index, or false otherwise.
-
 # Constraints:
 # 1 <= nums.length <= 104
 # 0 <= nums[i] <= 105
