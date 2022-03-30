bf810ce44fceeaf50aca36cd98d8ae21cba979c4
fattie <mpaulperr@gmail.com>
2022-03-30 15:47:17 -0400

    Subsets

--- practice/leetcode/top100/45.py
+++ practice/leetcode/top100/45.py
@@ -57,7 +57,6 @@ print(f"Testing {test2} returns {Solution.subsets(test2)} expected {expected2}")
 # https://leetcode.com/problems/subsets/
 # Given an integer array nums of unique elements, return all possible subsets (the power set).
 # The solution set must not contain duplicate subsets. Return the solution in any order. 
-
 # Constraints:
 # 1 <= nums.length <= 10
 # -10 <= nums[i] <= 10
