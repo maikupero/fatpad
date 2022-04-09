db92a40a9179409f6c8b90cbca18a6e94e9f7f81
fattie <mpaulperr@gmail.com>
2022-04-09 14:09:23 -0400

    initial styling & assets

--- studies/web_dev/weather_react/src/App.js
+++ studies/web_dev/weather_react/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+const API_KEY = process.env.api_key;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+      <div className="search-box">
+        <input 
+        type="text"
+        className="search-bar"
+        placeholder="Search..."
+        />
+      </div>
+      </main>
     </div>
   );
 }
