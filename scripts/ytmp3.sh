7f8e99135f77318b87e6f11a906b4b4951481367
fattie <mpaulperr@gmail.com>
2022-05-16 08:50:53 -0400

    youtube dl bash script!

---
+++ scripts/ytmp3.sh
@@ -0,0 +1,29 @@
+read -p 'URL: ' URL
+read -p 'Title: ' title
+read -p 'Artist: ' artist
+read -p 'Album: ' album
+read -p 'Genre: ' genre
+echo $URL $title $artist $genre
+youtube-dl -x --audio-format m4a --add-metadata --postprocessor-args "-metadata title='$title' -metadata artist='$artist' -metadata album='$album' -metadata genre='$genre'" --audio-quality 4 --abort-on-error --geo-bypass --mark-watched -o "fromyt.%(ext)s" $URL
+if [ -f fromyt.m4a ] 
+then 
+    echo "Opening in Music"
+    open -a Music fromyt.m4a
+    read -p "Press enter to delete and finish."
+    rm fromyt.m4a
+else
+    echo "Conversion didn't work, deleting the download file."
+    [ -f fromyt.webm.part ] && rm fromyt.webm.part || rm fromyt.webm
+fi
+
+# Simple script for my personal use, since I keep track of all my music. Custom metadata insertion, automatic file opening and download file deletion.
+
+# DOCUMENTATION
+# https://github.com/ytdl-org/youtube-dl/blob/master/README.md#readme
+
+# pip install youtube-dl
+# pip install ffmpeg
+# pip install ffprobe
+# Alternatively, brew for mac.
+
+# youtube-dl [OPTIONS] URL [URL...]
