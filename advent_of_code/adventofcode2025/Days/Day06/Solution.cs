2b16e5bc90e42b671b8b3a2d5dae03211f9eaf98
maikupero <mpaulperr@gmail.com>
2025-12-07 02:41:00 +1300

    Tiny cleanup for day 6

--- advent_of_code/adventofcode2025/Days/Day06/Solution.cs
+++ advent_of_code/adventofcode2025/Days/Day06/Solution.cs
@@ -30,20 +30,6 @@ public class Solution : IDay
     return (problemCols, operands);
   }
 
-  public string RunTheNumbersBoys((List<List<long>>, List<string>) problemData)
-  {
-    var (problemCols, operands) = problemData;
-    var answers = new long[problemCols.Count];
-    for (int i = 0; i < problemCols.Count; i++) // col
-    {
-      long answerTotal = operands[i] == "+" ? 0 : 1;
-      foreach (var num in problemCols[i])
-        answerTotal = operands[i] == "+" ? answerTotal + num : answerTotal * num;
-      answers[i] = answerTotal;
-    }
-    return answers.Sum().ToString();
-  }
-
   // 123 328  51 64      356    8  175    4
   //  45 64  387 23   ->  24  248  581  431
   //   6 98  215 314  ->   1  369   32  623
@@ -52,10 +38,11 @@ public class Solution : IDay
   {
     var filename = exampleSolved ? "Input.txt" : "Example.txt";
     var lines = File.ReadAllLines(Path.Combine(BasePath, filename));
-    Print(lines);
+
     var problemCols = new List<List<long>>();
-    var operandsRow = lines[lines.Length - 1];
     var operands = new List<string>();
+    var operandsRow = lines[lines.Length - 1];
+
     for (int i = 0; i < operandsRow.Length; i++) // parse through the bottom row
     {
       if (Equals(operandsRow[i], '+') || Equals(operandsRow[i], '*'))
@@ -71,6 +58,7 @@ public class Solution : IDay
         )
           searchForProblemXBound++;
         var problemXBound = searchForProblemXBound - 1; // account for 1 space gap between columns
+
         if (searchForProblemXBound == operandsRow.Length)
           problemXBound += 1; // stupid one off case for the very last space in input
 
@@ -79,15 +67,28 @@ public class Solution : IDay
           var cephapodNum = "";
           for (var y = 0; y < lines.Length - 1; y++)
             cephapodNum += lines[y][x];
-          Print("cephapodNum", cephapodNum);
           problemCol.Add(long.Parse(cephapodNum.Trim()));
         }
+
         problemCols.Add(problemCol);
       }
     }
-    Print(problemCols);
 
     return (problemCols, operands);
+  }
+
+  private static string RunTheNumbersBoys((List<List<long>>, List<string>) problemData)
+  {
+    var (problemCols, operands) = problemData;
+    var answers = new long[problemCols.Count];
+    for (int i = 0; i < problemCols.Count; i++) // col
+    {
+      long answerTotal = operands[i] == "+" ? 0 : 1;
+      foreach (var num in problemCols[i])
+        answerTotal = operands[i] == "+" ? answerTotal + num : answerTotal * num;
+      answers[i] = answerTotal;
+    }
+    return answers.Sum().ToString();
   }
 
   public string Part1()
